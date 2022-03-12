8dbe5305572debc7d0d8d0e418467866eeae1132
Erick Sarabia <erick.sarabia@skydropx.com>
2022-03-12 13:09:15 -0600

    chore: add custom webpack config to inject env

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,21 @@
+const injectEnv = (definitions) => {
+  const env = 'process.env';
+
+  if (!definitions[env]) {
+    return {
+      ...definitions,
+      [env]: JSON.stringify(
+        Object.fromEntries(
+          Object.entries(definitions)
+            .filter(([key]) => key.startsWith(env))
+            .map(([key, value]) => [key.substring(env.length + 1), JSON.parse(value)]),
+        ),
+      ),
+    };
+  }
+  return definitions;
+}
+
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
@@ -8,4 +26,14 @@ module.exports = {
   //   // Return the altered config
   //   return config;
   // },
+  webpackFinal: async config => {
+    const definePlugin = config.plugins.find(
+      ({ constructor }) => constructor && constructor.name === 'DefinePlugin',
+    );
+    if (definePlugin) {
+      definePlugin.definitions = injectEnv(definePlugin.definitions);
+    }
+
+    return config;
+  },
 };
